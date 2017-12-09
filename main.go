12a52bad6b2244ac5630e014d6bce5d254e108db
Rubén Caro <ruben.caro.estevez@gmail.com>
2017-12-09 19:15:20 +0100

    Minimal router

--- main.go
+++ main.go
@@ -1,9 +1,24 @@
 package main
 
 import (
-	"fmt"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/rubencaro/cardo/lib/web"
 )
 
 func main() {
-	fmt.Println("hello")
+
+	http.HandleFunc("/ping", web.PingHandler)
+	http.HandleFunc("/", http.NotFound)
+
+	// start server
+	srv := &http.Server{
+		Addr:         "localhost:8888",
+		WriteTimeout: 15 * time.Second,
+		ReadTimeout:  15 * time.Second,
+	}
+
+	log.Fatal(srv.ListenAndServe())
 }
